a387061674740b94507dd27f39aabc3f5581046d
yafantao <765370813@qq.com>
2024-03-06 21:54:06 +0800

    Set ETH as target

--- data.py
+++ data.py
@@ -339,15 +339,16 @@ class Data:
 def main():
     # symbol = "BTCUSDT"
     # symbol = "BTCBUSD"
-    symbol = "BTCFDUSD"
+    # symbol = "BTCFDUSD"
+    symbol = "ETHFDUSD"
     # symbol = "BTCTUSD"
     # symbol = "SOLBUSD"
     # symbol = "GMTBUSD"
     # symbol = "DOGEBUSD"
     # symbol = "1000LUNCBUSD"
     interval = DataType.INTERVAL_1MINUTE
-    start = "2023/9/9 00:00 UTC+8"
-    # start = "140 days ago UTC+8"
+    # start = "2023/9/9 00:00 UTC+8"
+    start = "80 days ago UTC+8"
     end = "1 minute ago UTC+8"
 
     data = Data(symbol, interval, is_futures=False)
